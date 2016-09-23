a6b388a5158bf678997da3c32f7806015c0dd093
Oleg Akbarov <olegakbarov@gmail.com>
2016-09-23 18:14:48 +0300

    removed dynamic sizing for days_row

--- src/cljs/nexus/templates/editor/days_row.cljs
+++ src/cljs/nexus/templates/editor/days_row.cljs
@@ -6,11 +6,11 @@
     [cljs.core.async :refer [<! put! chan timeout]]
     [nexus.chans :refer [scroll-chan cur-scroll-y prev-scroll-y]]))
 
-(def days-basic {:height "100px"
-                 :width "100px"
-                 :text-align "center"
-                 :line-height "100px"
-                 :font-size "33px"})
+; (def days-basic {:height "100px"
+;                  :width "100px"
+;                  :text-align "center"
+;                  :line-height "100px"
+;                  :font-size "33px"})
 
 (def days-folded {:width "40px"
                   :height "40px"
@@ -18,11 +18,11 @@
                   :line-height "40px"
                   :font-size "19px"})
 
-(def days-style (r/atom days-basic))
+(def days-style (r/atom days-folded))
 
-(def row-basic {:position "absolute"})
+; (def row-basic {:position "absolute"})
 (def row-folded {:position "fixed" :top "65px" :z-index 9999})
-(def row-style (r/atom row-basic))
+(def row-style (r/atom row-folded))
 
 (defn days [items]
   [:div.days_wrapper {:style @row-style}
@@ -33,18 +33,18 @@
 (defn days-row []
   [days (range 7)])
 
-(defn listen! []
-  (let [chan (scroll-chan)]
-    (go-loop []
-         (let [y (<! chan)]
-           (reset! prev-scroll-y @cur-scroll-y)
-           (if (> y 0)
-             (do
-               (reset! row-style row-folded)        ;; This sucks.
-               (reset! days-style days-folded))
-             (do
-               (reset! row-style row-basic)
-               (reset! days-style days-basic))))
-         (recur))))
-
-(listen!)
+; (defn listen! []
+;   (let [chan (scroll-chan)]
+;     (go-loop []
+;          (let [y (<! chan)]
+;            (reset! prev-scroll-y @cur-scroll-y)
+;            (if (> y 0)
+;              (do
+;                (reset! row-style row-folded)        ;; This sucks.
+;                (reset! days-style days-folded))
+;              (do
+;                (reset! row-style row-basic)
+;                (reset! days-style days-basic))))
+;          (recur))))
+;
+; (listen!)
